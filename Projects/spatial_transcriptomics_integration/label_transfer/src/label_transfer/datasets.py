61343cbed6b702f5e4ce430a8658c0c90a9c3e4e
egerc <153100377+egerc@users.noreply.github.com>
2025-11-26 10:14:48 +0100

    .

--- Projects/spatial_transcriptomics_integration/label_transfer/src/label_transfer/datasets.py
+++ Projects/spatial_transcriptomics_integration/label_transfer/src/label_transfer/datasets.py
@@ -1,8 +1,21 @@
+from functools import partial
 from typing import Any, Callable, Generator
 
+from anndata import AnnData
 from exp_runner import Variable
-from .utils import Dataset
+from label_transfer.utils import Dataset
+from nico2_lib.datasets import xenium_10x_loader
 
+dataset_pairings = [
+    ()
+]
 
-
-def dataset_generator() -> Generator[Variable[Callable[[], Dataset]], Any, None]: ...
+def dataset_generator() -> Generator[Variable[Callable[[], Dataset]], Any, None]:
+    def human_liver_10x() -> Dataset:
+        query = xenium_10x_loader("Xenium_V1_hLiver_nondiseased_section_FFPE")
+        query_ct_key: str = ...
+        reference: AnnData = ...
+        reference_ct_key: str = ...
+        return Dataset(
+            query, reference, query_ct_key, reference_ct_key
+        )
